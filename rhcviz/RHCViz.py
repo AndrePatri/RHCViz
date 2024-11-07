dedb8d92bdec37ff8fa350ef816df7bcf7ddcf2e
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-11-07 17:17:59 +0100

    added transforms for moving frames

--- rhcviz/RHCViz.py
+++ rhcviz/RHCViz.py
@@ -493,8 +493,9 @@ class RHCViz:
         Publish rhc state to rviz
         """
         # Publish base pose
+        now=rospy.Time.now()
         transform = TransformStamped()
-        transform.header.stamp = rospy.Time.now()
+        transform.header.stamp = now
         transform.header.frame_id = 'world'
         transform.child_frame_id = f'{self.nodes_tf_prefixes[node_index]}/{self.baselink_name}'
         transform.transform.translation.x = base_pose[0]
@@ -506,6 +507,21 @@ class RHCViz:
         transform.transform.rotation.w = base_pose[6]
 
         self.tf_broadcaster.sendTransform(transform)
+        
+        # publish a frame which is below the robot base, on the ground and world oriented
+        moving_frame_transform = TransformStamped()
+        moving_frame_transform.header.stamp = now
+        moving_frame_transform.header.frame_id = 'world'
+        moving_frame_transform.child_frame_id = f'{self.state_tf_prefix}/{self.moving_robot_fname}'
+        moving_frame_transform.transform.translation.x = base_pose[0]
+        moving_frame_transform.transform.translation.y = base_pose[1]
+        moving_frame_transform.transform.translation.z = 0.0
+        moving_frame_transform.transform.rotation.x = 0.0
+        moving_frame_transform.transform.rotation.y = 0.0
+        moving_frame_transform.transform.rotation.z = 0.0
+        moving_frame_transform.transform.rotation.w = 1.0
+
+        self.tf_broadcaster.sendTransform(moving_frame_transform)
 
         # Publish joint positions
         joint_state = JointState()
@@ -552,9 +568,10 @@ class RHCViz:
         """
         Publish robot state to rviz
         """
+        now=rospy.Time.now()
         # Publish base pose
         transform = TransformStamped()
-        transform.header.stamp = rospy.Time.now()
+        transform.header.stamp =now
         transform.header.frame_id = 'world'
         transform.child_frame_id = f'{self.state_tf_prefix}/{self.baselink_name}'
         transform.transform.translation.x = base_pose[0]
@@ -566,6 +583,20 @@ class RHCViz:
         transform.transform.rotation.w = base_pose[6]
 
         self.tf_broadcaster.sendTransform(transform)
+
+        # publish a frame which is below the robot base, on the ground and world oriented
+        moving_frame_transform = TransformStamped()
+        moving_frame_transform.header.stamp = now
+        moving_frame_transform.header.frame_id = 'world'
+        moving_frame_transform.child_frame_id = f'{self.state_tf_prefix}/{self.moving_robot_fname}'
+        moving_frame_transform.transform.translation.x = base_pose[0]
+        moving_frame_transform.transform.translation.y = base_pose[1]
+        moving_frame_transform.transform.translation.z = 0.0
+        moving_frame_transform.transform.rotation.x = 0.0
+        moving_frame_transform.transform.rotation.y = 0.0
+        moving_frame_transform.transform.rotation.z = 0.0
+        moving_frame_transform.transform.rotation.w = 1.0
+        self.tf_broadcaster.sendTransform(moving_frame_transform)
 
         # Publish joint positions
         joint_state = JointState()

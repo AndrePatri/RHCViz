b6449c33faabb467678607a2f463bc216717b2dc
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-12-10 15:20:16 +0100

    fixed wrong names being used for robot jnt state

--- rhcviz/RHCViz.py
+++ rhcviz/RHCViz.py
@@ -602,7 +602,7 @@ class RHCViz:
         joint_state = JointState()
         joint_state.header.stamp = rospy.Time.now()
         if self._check_jnt_names:
-            joint_state.name = self.joint_names_rhc
+            joint_state.name = self.robot_joint_names
         else:
             # we use the one parsed from the urdf (dangerous)
             joint_state.name = self.joint_names_urdf
